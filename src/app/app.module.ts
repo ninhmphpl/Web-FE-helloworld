ccf85000b84de0d1eff7ea921126fe4ae5c71289
NinhPc <ninhmphpl@gmail.com>
2023-02-11 17:56:53 +0700

    tai len giao diện người dùng, component bao gồm, bao gồm các component header, nav bar, footer , Lọc, Search, Thể loại,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavComponent } from './nav/nav.component';
+import { ProductComponent } from './product/product.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoryComponent } from './category/category.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    ProductComponent,
+    FooterComponent,
+    CategoryComponent,
+    CarouselComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
