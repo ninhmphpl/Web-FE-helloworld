3d8e2589463381244526367d7148255938c9ef97
NinhPc <ninhmphpl@gmail.com>
2023-02-11 22:19:16 +0700

    ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "employee" , children : [
+    {path : '' , component : ProductListComponent}
+  ]},
+  {path : '', component : ProductListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductListPageService } from '../service/product-list-page.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
+
+  constructor(
+    public pageService : ProductListPageService
+  ){}
+
+  ngOnInit(): void {
+    this.pageService.getListProduct(0);
+  }
+
 
 }
