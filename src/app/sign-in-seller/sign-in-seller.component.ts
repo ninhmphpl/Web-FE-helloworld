d958e6430f01fd21a8960fc9147e8e69681525e7
NinhPc <ninhmphpl@gmail.com>
2023-02-15 22:20:35 +0700

    su thong tin san pham

--- src/app/sign-in-seller/sign-in-seller.component.ts
+++ src/app/sign-in-seller/sign-in-seller.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environtment } from 'src/environments/environtment';
 
 @Component({
   selector: 'app-sign-in-seller',
   templateUrl: './sign-in-seller.component.html',
   styleUrls: ['./sign-in-seller.component.scss']
 })
-export class SignInSellerComponent {
+export class SignInSellerComponent implements OnInit{
+  role! : string;
+  ngOnInit(): void {
+    this.role = environtment.role
+  }
 
 }
