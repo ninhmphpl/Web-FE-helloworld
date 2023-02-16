8b7bd5e707e8a07379c6a948b7d24ac43135c49c
NinhPc <ninhmphpl@gmail.com>
2023-02-17 06:22:20 +0700

    cap nhat nut bam

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailComponent } from "./product-detail/product-detail.component";
 import { ProductCreateComponent } from "./product-create/product-create.component";
 import { PageErrorComponent } from './page-error/page-error.component';
-import {AdminDisplayEmployeeComponent} from "./admin-display-employee/admin-display-employee.component";
 import {AdminComponent} from "./admin/admin.component";
 import {DetailEmployeeComponent} from "./detail-employee/detail-employee.component";
 import {FormEmployeeComponent} from "./form-employee/form-employee.component";
